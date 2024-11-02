e3386597f37af31b6f4f5a39a4baa4ce60a709d5
HeroBuxx <me@herobuxx.me>
2024-11-03 01:36:32 +0700

    api: helper: Create a new helper to parse token from header and return user data
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- helper/watoken/wauser.go
+++ helper/watoken/wauser.go
@@ -1,1 +1,36 @@
 package watoken
+
+import (
+	"github.com/gocroot/config"
+	"github.com/gocroot/helper/at"
+	"github.com/gocroot/helper/atdb"
+	"github.com/gocroot/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
+)
+
+func ParseToken(w http.ResponseWriter, r *http.Request) (docuser model.Userdomyikado, err error) {
+	payload, err := Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(r))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(r)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(w, http.StatusForbidden, respn)
+		return
+	}
+
+	// Ambil data pengguna berdasarkan nomor telepon dari payload
+	docuser, err = atdb.GetOneDoc[model.Userdomyikado](config.Mongoconn, "user", primitive.M{"phonenumber": payload.Id})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Pengguna tidak ditemukan"
+		respn.Info = payload.Id
+		respn.Location = "Get User Data"
+		respn.Response = err.Error()
+		at.WriteJSON(w, http.StatusNotFound, respn)
+		return
+	}
+	return docuser, err
+}
