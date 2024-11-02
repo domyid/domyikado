060bc93c3003f1e9367df3f57942563d3b7309b9
HeroBuxx <me@herobuxx.me>
2024-11-03 02:45:20 +0700

    api: model: Drop unused data struct
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- model/stats.go
+++ model/stats.go
@@ -2,11 +2,6 @@ package model
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type StatsRes0 struct {
-	UserID  primitive.ObjectID `json:"userid"`
-	Commits int64              `json:"commits"`
-}
-
 type StatData struct {
 	ProjectID primitive.ObjectID `json:"projectid"`
 	Count     int64              `json:"count"`
