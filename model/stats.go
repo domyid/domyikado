86c5a16efd738d7a24856fc940e144167d5c6c2f
HeroBuxx <me@herobuxx.me>
2024-11-03 02:34:28 +0700

    api: model: stats: Create new reponse data struct
    To show counts of each projects instead total
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- model/stats.go
+++ model/stats.go
@@ -6,3 +6,14 @@ type StatsRes0 struct {
 	UserID  primitive.ObjectID `json:"userid"`
 	Commits int64              `json:"commits"`
 }
+
+type StatData struct {
+	ProjectID primitive.ObjectID `json:"projectid"`
+	Count     int64              `json:"count"`
+}
+
+type CountResponse struct {
+	UserID     primitive.ObjectID `json:"userid"`
+	Projects   []StatData         `json:"projects"`
+	TotalCount int64              `json:"total"`
+}
