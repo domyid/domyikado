aa21ce4a19da1d6070ea8e94c43a69889d4bdfdf
HeroBuxx <me@herobuxx.me>
2024-11-03 01:59:52 +0700

    api: stats: Create controller for Feedback count
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- controller/stats.go
+++ controller/stats.go
@@ -42,3 +42,34 @@ func CountCommits(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Gagal mengirim data dalam format JSON: "+err.Error(), http.StatusInternalServerError)
 	}
 }
+
+func CountFeedback(w http.ResponseWriter, r *http.Request) {
+	docuser, err := watoken.ParseToken(w, r)
+	if err != nil {
+		return
+	}
+
+	commitCount, err := atdb.GetCountDoc(config.Mongoconn, "uxlaporan", primitive.M{"nopetugas": docuser.PhoneNumber})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Data project tidak di temukan"
+		respn.Response = err.Error()
+		at.WriteJSON(w, http.StatusNotFound, respn)
+		return
+	}
+	// Jika tidak ada data ditemukan
+	if commitCount == 0 {
+		http.Error(w, "Tidak ada data yang ditemukan untuk pengguna ini", http.StatusNotFound)
+		return
+	}
+
+	var countResp model.StatsRes0
+	countResp.UserID = docuser.ID
+	countResp.Commits = commitCount
+
+	// Menulis respons dalam format JSON
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(countResp); err != nil {
+		http.Error(w, "Gagal mengirim data dalam format JSON: "+err.Error(), http.StatusInternalServerError)
+	}
+}
