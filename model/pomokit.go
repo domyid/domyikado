7dabda77f6f1908c64cfe635320cd1358cc4b18c
raffzhm <rafiazhim.ra@gmail.com>
2025-03-12 06:22:32 +0700

    tambah model data pomokit

---
+++ model/pomokit.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type PomodoroReport struct {
+    ID            primitive.ObjectID `bson:"_id,omitempty"`
+	Owner         Userdomyikado      `bson:"owner"`
+    PhoneNumber   string             `bson:"phonenumber,omitempty"`
+    Cycle         int                `bson:"cycle"`
+    Hostname      string             `bson:"hostname"`
+    IP            string             `bson:"ip"`
+    Screenshots   int                `bson:"screenshots"`
+    Pekerjaan     string             `bson:"pekerjaan"`
+    Token         string             `bson:"token"`
+    URLPekerjaan  string             `bson:"urlpekerjaan"`
+    CreatedAt     time.Time          `bson:"createdAt"`
+}
