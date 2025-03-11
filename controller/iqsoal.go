dc05c2487ec86c813a177c5041e2c3e23fcf974f
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-03-11 07:42:11 +0700

    update controller iq

--- controller/iqsoal.go
+++ controller/iqsoal.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gocroot/config"
 	"github.com/gocroot/helper/at"
+	"github.com/gocroot/helper/atdb"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,6 +32,7 @@ func GetIqQuestions(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Query MongoDB
+	var iqQuestions []SoalIQ
 	cursor, err := config.Mongoconn.Collection("questioniq").Find(context.Background(), filter, options.Find())
 	if err != nil {
 		log.Printf("Error querying IQ questions: %v", err)
@@ -41,20 +44,20 @@ func GetIqQuestions(w http.ResponseWriter, r *http.Request) {
 	}
 	defer cursor.Close(context.Background())
 
-	var iqQuestions []SoalIQ
+	//Iterasi cursor
 	for cursor.Next(context.Background()) {
-		var question SoalIQ
-		if err := cursor.Decode(&question); err != nil {
-			log.Printf("Error decoding IQ question: %v", err)
+		var iqQuestion SoalIQ
+		err := cursor.Decode(&iqQuestion)
+		if err != nil {
+			log.Printf("Error decoding IQ questions: %v", err)
 			at.WriteJSON(w, http.StatusInternalServerError, map[string]string{
-				"message": "Error decoding IQ question",
+				"message": "Error decoding IQ questions",
 				"error":   err.Error(),
 			})
 			return
 		}
-		iqQuestions = append(iqQuestions, question)
+		iqQuestions = append(iqQuestions, iqQuestion)
 	}
-
 	if len(iqQuestions) == 0 {
 		at.WriteJSON(w, http.StatusNotFound, map[string]string{
 			"message": "Tidak ada soal IQ yang tersedia.",
@@ -63,4 +66,30 @@ func GetIqQuestions(w http.ResponseWriter, r *http.Request) {
 	}
 
 	at.WriteJSON(w, http.StatusOK, iqQuestions)
+}
+
+// GetOneIqQuestion retrieves a single IQ question from the MongoDB collection by ID
+func GetOneIqQuestion(w http.ResponseWriter, r *http.Request) {
+	// Mendapatkan ID dari URL
+	pathSegments := strings.Split(r.URL.Path, "/")
+	id := pathSegments[len(pathSegments)-1]
+
+	// Filter untuk mendapatkan soal IQ berdasarkan ID
+	filter := bson.M{
+		"id":         id,
+		"deleted_at": bson.M{"$exists": false}, // Pastikan soal belum dihapus
+	}
+
+	// GetOneDoc dari helper atdb
+	iqQuestion, err := atdb.GetOneDoc[SoalIQ](config.Mongoconn, "questioniq", filter)
+	if err != nil {
+		log.Printf("Error querying IQ question with ID %s: %v", id, err)
+		at.WriteJSON(w, http.StatusNotFound, map[string]string{
+			"message": "Soal IQ tidak ditemukan.",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	at.WriteJSON(w, http.StatusOK, iqQuestion)
 }
