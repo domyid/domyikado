a5ee2e970aadb2c5bb5a8d2c918be43e01d912c1
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-03-11 10:31:18 +0700

    coba

--- controller/iqsoal.go
+++ controller/iqsoal.go
@@ -11,15 +11,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-// Struct SoalIQ sesuai dengan koleksi questioniq
+// Struct SoalIQ sesuai dengan koleksi iqquestion
 type SoalIQ struct {
-	ID        string `json:"id"`
-	Question  string `json:"question"`
-	Image     string `json:"image"`
-	AnswerKey string `json:"answer_key"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-	DeletedAt string `json:"deleted_at"`
+	ID        string  `json:"id" bson:"id"`
+	Question  string  `json:"question" bson:"question"`
+	Image     string  `json:"image" bson:"image"`
+	AnswerKey string  `json:"answer_key" bson:"answer_key"`
+	CreatedAt string  `json:"created_at" bson:"created_at"`
+	UpdatedAt *string `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	DeletedAt *string `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
 }
 
 // GetOneIqQuestion retrieves a single IQ question from the MongoDB collection by ID
@@ -27,10 +27,14 @@ func GetOneIqQuestion(w http.ResponseWriter, r *http.Request) {
 	// Mendapatkan ID dari URL
 	pathSegments := strings.Split(r.URL.Path, "/")
 	id := pathSegments[len(pathSegments)-1]
+
 	// Filter untuk mendapatkan soal IQ berdasarkan ID
 	filter := bson.M{
-		"id":         id,
-		"deleted_at": bson.M{"$exists": false},
+		"id": id,
+		"$or": []bson.M{
+			{"deleted_at": nil},
+			{"deleted_at": bson.M{"$exists": false}},
+		},
 	}
 
 	var iqQuestion SoalIQ
