604cd73bd9abf8fa84aaf8b42dd5e38af0a50940
Febriand1 <fdirga63@gmail.com>
2025-03-12 18:31:28 +0700

    coba buat struct dari json

--- controller/strava.go
+++ controller/strava.go
@@ -2,15 +2,31 @@ package controller
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gocroot/helper/at"
 	"github.com/gocroot/helper/atapi"
 	"github.com/gocroot/model"
 )
 
+type StravaActivity struct {
+	ActivityId   string    `bson:"activity_id" json:"activity_id"`
+	Picture      string    `bson:"picture" json:"picture"`
+	Name         string    `bson:"name" json:"name"`
+	Title        string    `bson:"title" json:"title"`
+	DateTime     string    `bson:"date_time" json:"date_time"`
+	TypeSport    string    `bson:"type_sport" json:"type_sport"`
+	Distance     string    `bson:"distance" json:"distance"`
+	MovingTime   string    `bson:"moving_time" json:"moving_time"`
+	Elevation    string    `bson:"elevation" json:"elevation"`
+	LinkActivity string    `bson:"link_activity" json:"link_activity"`
+	Status       string    `bson:"status" json:"status"`
+	CreatedAt    time.Time `bson:"created_at" json:"created_at"`
+}
+
 func GetStravaActivities(respw http.ResponseWriter, req *http.Request) {
 	api := "https://asia-southeast1-awangga.cloudfunctions.net/wamyid/strava/activities"
-	scode, doc, err := atapi.Get[[]model.Response](api)
+	scode, doc, err := atapi.Get[StravaActivity](api)
 	if err != nil {
 		at.WriteJSON(respw, scode, model.Response{Response: err.Error()})
 		return
