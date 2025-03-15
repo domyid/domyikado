15a8dd253dd12808f57b7e4f74175c49690c7765
raffzhm <rafiazhim.ra@gmail.com>
2025-03-15 19:45:12 +0700

    update route rekap pomokit

--- route/route.go
+++ route/route.go
@@ -186,6 +186,16 @@ func URL(w http.ResponseWriter, r *http.Request) {
 		controller.GetStravaData(w, r)
 	case method == "GET" && path == "/refresh/reportmingguan":
 		controller.GetNewCode(w, r)
+
+
+	// case method == "GET" && path == "/refresh/report/pomokit/harian":
+	// 	controller.GetPomokitDailyReport(w, r)
+	case method == "GET" && path == "/refresh/report/pomokit/mingguan":
+		controller.GetPomokitWeeklySummary(w, r)
+	case method == "GET" && path == "/data/pomokit/report":
+		controller.GetPomokitReport(w, r)
+	case method == "GET" && path == "/data/pomokit/batch":
+		controller.BatchPomokitProcess(w, r)
 	default:
 		controller.NotFound(w, r)
 	}
