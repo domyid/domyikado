ad656c6b5e83a3d306d75ba4fb662849aed12141
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-16 19:34:01 +0700

    Tambah url

--- model/tracker.go
+++ model/tracker.go
@@ -5,6 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type UserInfo struct {
 	IPv4          string             `json:"ipv4"`
 	Hostname      string             `json:"hostname"`
+	Url           string             `json:"url"`
 	Browser       string             `json:"browser"`
 	Tanggal_Ambil primitive.DateTime `json:"tanggal_ambil"`
 }
