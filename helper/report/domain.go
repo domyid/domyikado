04d6005cedb9c89cdd2b4f80acbac4e54f51b436
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-16 20:50:47 +0700

    Tambah data domain nomor hp manual

---
+++ helper/report/domain.go
@@ -0,0 +1,249 @@
+package report
+
+type PhoneDomain struct {
+	PhoneNumber      string
+	Project_Hostname string
+}
+
+var domainProyek1 = []PhoneDomain{
+	{
+		PhoneNumber:      "62895619421397",
+		Project_Hostname: "iyannn03.github.io",
+	},
+	{
+		PhoneNumber:      "6285277102907",
+		Project_Hostname: "samuelsidabutar.github.io",
+	},
+	{
+		PhoneNumber:      "62895605870596",
+		Project_Hostname: "rafliputrap.github.io",
+	},
+	{
+		PhoneNumber:      "6281288951726",
+		Project_Hostname: "zahraramanayshilla.github.io",
+	},
+	{
+		PhoneNumber:      "6281319224204",
+		Project_Hostname: "wanawan-bismillahsemangat.github.io",
+	},
+	{
+		PhoneNumber:      "6281211437153",
+		Project_Hostname: "affifahputri.github.io",
+	},
+	{
+		PhoneNumber:      "6285283751548",
+		Project_Hostname: "aliaawz.github.io",
+	},
+	{
+		PhoneNumber:      "6285722341788",
+		Project_Hostname: "fauzinrfdlh10.github.io",
+	},
+	{
+		PhoneNumber:      "6281382626096",
+		Project_Hostname: "saripudin14.github.io",
+	},
+	{
+		PhoneNumber:      "6285157392215",
+		Project_Hostname: "bagus0630.github.io",
+	},
+	{
+		PhoneNumber:      "6287723806103",
+		Project_Hostname: "ameringkseh.github.io",
+	},
+	{
+		PhoneNumber:      "6285158354600",
+		Project_Hostname: "saha-hub123.github.io",
+	},
+	{
+		PhoneNumber:      "6289643038119",
+		Project_Hostname: "titangnwn.github.io",
+	},
+	{
+		PhoneNumber:      "6282122339125",
+		Project_Hostname: "muhammadmallq.github.io",
+	},
+	{
+		PhoneNumber:      "6281366299487",
+		Project_Hostname: "muhammadbaihaqi0.github.io",
+	},
+	{
+		PhoneNumber:      "62895328739927",
+		Project_Hostname: "rr-php.github.io",
+	},
+	{
+		PhoneNumber:      "6285179935117",
+		Project_Hostname: "gas.renz.my.id",
+	},
+	{
+		PhoneNumber:      "6282258120851",
+		Project_Hostname: "alifyazz.github.io",
+	},
+	{
+		PhoneNumber:      "6285314627451",
+		Project_Hostname: "kucingnanda.github.io",
+	},
+	{
+		PhoneNumber:      "6289622700077",
+		Project_Hostname: "ugihub.github.io",
+	},
+	{
+		PhoneNumber:      "6285860602948",
+		Project_Hostname: "rdityaa.github.io",
+	},
+	{
+		PhoneNumber:      "62895356871030",
+		Project_Hostname: "nawal886.github.io",
+	},
+	{
+		PhoneNumber:      "6289604239732",
+		Project_Hostname: "raihanhidayat02.github.io",
+	},
+	{
+		PhoneNumber:      "6281222267652",
+		Project_Hostname: "sams-js.github.io",
+	},
+	{
+		PhoneNumber:      "6281463918825",
+		Project_Hostname: "zarasabilaa.github.io",
+	},
+	{
+		PhoneNumber:      "6282116301579",
+		Project_Hostname: "t.if.co.id/714240037/",
+	},
+	{
+		PhoneNumber:      "62895329746650",
+		Project_Hostname: "isminabilah345.github.io",
+	},
+	{
+		PhoneNumber:      "6283861161768",
+		Project_Hostname: "keylasun.github.io",
+	},
+	{
+		PhoneNumber:      "6288970788847",
+		Project_Hostname: "venalism.github.io",
+	},
+	{
+		PhoneNumber:      "6282293083116",
+		Project_Hostname: "haikal5352.github.io",
+	},
+	{
+		PhoneNumber:      "6287716724977",
+		Project_Hostname: "zahranur4.github.io",
+	},
+	{
+		PhoneNumber:      "6285883077895",
+		Project_Hostname: "daparamadhan.github.io",
+	},
+	{
+		PhoneNumber:      "6289660770210",
+		Project_Hostname: "fiqryf.github.io",
+	},
+	{
+		PhoneNumber:      "6288218271426",
+		Project_Hostname: "pai.renz.my.id",
+	},
+	{
+		PhoneNumber:      "6281265456058",
+		Project_Hostname: "muelharr.github.io",
+	},
+	{
+		PhoneNumber:      "6282232023945",
+		Project_Hostname: "wahyu8129.github.io",
+	},
+	{
+		PhoneNumber:      "6285779371516",
+		Project_Hostname: "farhan123853.github.io",
+	},
+	{
+		PhoneNumber:      "6289604482359",
+		Project_Hostname: "ridwanhakimr.github.io",
+	},
+	{
+		PhoneNumber:      "6287735318207",
+		Project_Hostname: "nathye3.github.io",
+	},
+	{
+		PhoneNumber:      "6289662315611",
+		Project_Hostname: "rahmaauliakh.github.io",
+	},
+	{
+		PhoneNumber:      "6283114925705",
+		Project_Hostname: "zidanhairraramadhan.github.io",
+	},
+	{
+		PhoneNumber:      "6285759790334",
+		Project_Hostname: "itzzahra16.github.io",
+	},
+	{
+		PhoneNumber:      "6283116012108",
+		Project_Hostname: "aidahmarchtiana.github.io",
+	},
+	{
+		PhoneNumber:      "6285759778650",
+		Project_Hostname: "zygadedx.github.io",
+	},
+	{
+		PhoneNumber:      "6288706468109",
+		Project_Hostname: "faidilzahpar.github.io",
+	},
+	{
+		PhoneNumber:      "6282191365504",
+		Project_Hostname: "t.if.co.id/714240039/",
+	},
+	{
+		PhoneNumber:      "6283137760847",
+		Project_Hostname: "t.if.co.id/714240058/",
+	},
+	{
+		PhoneNumber:      "6281312844675",
+		Project_Hostname: "yonaldiep.github.io",
+	},
+	{
+		PhoneNumber:      "6282119378629",
+		Project_Hostname: "mochammadraihanhamdan.github.io",
+	},
+	{
+		PhoneNumber:      "6285640389741",
+		Project_Hostname: "zidanardi.github.io",
+	},
+	{
+		PhoneNumber:      "6282245870175",
+		Project_Hostname: "ysmndiyah.github.io",
+	},
+	{
+		PhoneNumber:      "6285795139858",
+		Project_Hostname: "dika33.github.io",
+	},
+	{
+		PhoneNumber:      "6281992269526",
+		Project_Hostname: "fatimahtjl.github.io",
+	},
+	{
+		PhoneNumber:      "6289685587547",
+		Project_Hostname: "copetpasarsenin.github.io",
+	},
+	{
+		PhoneNumber:      "6282161299141",
+		Project_Hostname: "nogi3201.github.io",
+	},
+	{
+		PhoneNumber:      "6285725703050",
+		Project_Hostname: "averouse.github.io",
+	},
+	{
+		PhoneNumber:      "6285156620940",
+		Project_Hostname: "nuthfih.github.io",
+	},
+	{
+		PhoneNumber:      "6288807554616",
+		Project_Hostname: "fatimahtjl.github.io",
+	},
+	{
+		PhoneNumber:      "6285721368639",
+		Project_Hostname: "revania17.github.io",
+	},
+	{
+		PhoneNumber:      "62895351788818",
+		Project_Hostname: "azitriyadi.github.io",
+	},
+}
