6774d49d21db0e06c2f2b27b2623c5c57c9cc77b
Febriand1 <fdirga63@gmail.com>
2025-03-22 19:26:32 +0700

    tambah field name

--- controller/strava.go
+++ controller/strava.go
@@ -23,23 +23,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type StravaActivity struct {
-	AthleteId    string    `bson:"athlete_id" json:"athlete_id"`
-	ActivityId   string    `bson:"activity_id" json:"activity_id"`
-	Picture      string    `bson:"picture" json:"picture"`
-	Name         string    `bson:"name" json:"name"`
-	PhoneNumber  string    `bson:"phone_number" json:"phone_number"`
-	Title        string    `bson:"title" json:"title"`
-	DateTime     string    `bson:"date_time" json:"date_time"`
-	TypeSport    string    `bson:"type_sport" json:"type_sport"`
-	Distance     string    `bson:"distance" json:"distance"`
-	MovingTime   string    `bson:"moving_time" json:"moving_time"`
-	Elevation    string    `bson:"elevation" json:"elevation"`
-	LinkActivity string    `bson:"link_activity" json:"link_activity"`
-	Status       string    `bson:"status" json:"status"`
-	CreatedAt    time.Time `bson:"created_at" json:"created_at"`
-}
-
 // Daftar grup ID yang diperbolehkan
 var allowedGroups = map[string]bool{
 	"120363022595651310": true,
@@ -57,7 +40,7 @@ func ProcessStravaPoints(respw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	scode, activities, err := atapi.Get[[]StravaActivity](conf.StravaUrl)
+	scode, activities, err := atapi.Get[[]model.StravaActivity](conf.StravaUrl)
 	if err != nil || scode != http.StatusOK {
 		at.WriteJSON(respw, http.StatusInternalServerError, model.Response{Response: "Failed to fetch data"})
 		return
@@ -70,6 +53,7 @@ func ProcessStravaPoints(respw http.ResponseWriter, req *http.Request) {
 	userData := make(map[string]struct {
 		TotalKm       float64
 		ActivityCount int
+		NameStrava    string
 	})
 
 	for _, activity := range activities {
@@ -88,9 +72,11 @@ func ProcessStravaPoints(respw http.ResponseWriter, req *http.Request) {
 		userData[activity.PhoneNumber] = struct {
 			TotalKm       float64
 			ActivityCount int
+			NameStrava    string
 		}{
 			TotalKm:       userData[activity.PhoneNumber].TotalKm + distance,
 			ActivityCount: userData[activity.PhoneNumber].ActivityCount + 1,
+			NameStrava:    activity.Name,
 		}
 	}
 
@@ -110,6 +96,7 @@ func ProcessStravaPoints(respw http.ResponseWriter, req *http.Request) {
 			ActivityCount int                `bson:"activity_count"`
 			WaGroupID     string             `bson:"wagroupid,omitempty"`
 			UserID        primitive.ObjectID `bson:"user_id,omitempty"`
+			NameStrava    string             `bson:"name_strava,omitempty"`
 		}
 		err := colPoin.FindOne(context.TODO(), filter).Decode(&existing)
 		if err != nil && err != mongo.ErrNoDocuments {
@@ -119,7 +106,8 @@ func ProcessStravaPoints(respw http.ResponseWriter, req *http.Request) {
 
 		// Ambil user_id dari koleksi users berdasarkan phone_number
 		var user struct {
-			ID primitive.ObjectID `bson:"_id"`
+			ID   primitive.ObjectID `bson:"_id"`
+			Name string             `bson:"name"`
 		}
 		err = colUsers.FindOne(context.TODO(), bson.M{"phonenumber": phone}).Decode(&user)
 		if err != nil && err != mongo.ErrNoDocuments {
@@ -138,11 +126,13 @@ func ProcessStravaPoints(respw http.ResponseWriter, req *http.Request) {
 
 		update := bson.M{
 			"$set": bson.M{
-				"total_km":   math.Round(data.TotalKm*10) / 10,
-				"count":      existing.ActivityCount + data.ActivityCount,
-				"wagroupid":  selectedGroup,
-				"user_id":    user.ID, // Disimpan sebagai ObjectID
-				"updated_at": time.Now(),
+				"total_km":       math.Round(data.TotalKm*10) / 10,
+				"activity_count": existing.ActivityCount + data.ActivityCount,
+				"wagroupid":      selectedGroup,
+				"user_id":        user.ID, // Disimpan sebagai ObjectID
+				"updated_at":     time.Now(),
+				"name":           user.Name,       // Menyimpan nama user dari koleksi users
+				"name_strava":    data.NameStrava, // Menyimpan nama Strava dari aktivitas
 			},
 			"$inc": bson.M{
 				"poin": math.Round((data.TotalKm/6)*100*10) / 10,
@@ -163,6 +153,7 @@ type AddPointsRequest struct {
 	ActivityID  string  `json:"activity_id"`
 	PhoneNumber string  `json:"phone_number"`
 	Distance    float64 `json:"distance"`
+	NameStrava  string  `json:"name_strava"`
 }
 
 func AddStravaPoints(respw http.ResponseWriter, req *http.Request) {
@@ -193,7 +184,8 @@ func AddStravaPoints(respw http.ResponseWriter, req *http.Request) {
 
 	// Ambil data user_id berdasarkan phone_number
 	var user struct {
-		ID primitive.ObjectID `bson:"_id"`
+		ID   primitive.ObjectID `bson:"_id"`
+		Name string             `bson:"name"`
 	}
 	err = colUsers.FindOne(context.TODO(), bson.M{"phonenumber": reqBody.PhoneNumber}).Decode(&user)
 	if err != nil && err != mongo.ErrNoDocuments {
@@ -225,8 +217,10 @@ func AddStravaPoints(respw http.ResponseWriter, req *http.Request) {
 			"count":    1, // Menambahkan count aktivitas
 		},
 		"$set": bson.M{
-			"updated_at": time.Now(),
-			"user_id":    user.ID,
+			"updated_at":  time.Now(),
+			"user_id":     user.ID,
+			"name":        user.Name, // Simpan nama dari koleksi user
+			"name_strava": reqBody.NameStrava,
 		},
 	}
 	opts := options.Update().SetUpsert(true)
