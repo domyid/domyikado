b222362fdd84ec15acb56efdaa4cfd0ff53bd56a
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-04-18 11:51:41 +0700

    add controller bimbingan

---
+++ controller/bimbingan.go
@@ -0,0 +1,130 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/gocroot/config"
+	"github.com/gocroot/helper/at"
+	"github.com/gocroot/helper/atapi"
+	"github.com/gocroot/helper/atdb"
+	"github.com/gocroot/helper/report"
+	"github.com/gocroot/helper/watoken"
+	"github.com/gocroot/helper/whatsauth"
+	"github.com/gocroot/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func PostDosenAsesor(respw http.ResponseWriter, req *http.Request) {
+	//otorisasi dan validasi inputan
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+	var lap report.Laporan
+	err = json.NewDecoder(req.Body).Decode(&lap)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Body tidak valid"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusBadRequest, respn)
+		return
+	}
+	if lap.Phone == "" || lap.Nama == "" || lap.Solusi == "" {
+		var respn model.Response
+		respn.Status = "Error : Telepon atau nama atau solusi tidak diisi"
+		respn.Response = "Isi lebih lengkap dahulu"
+		at.WriteJSON(respw, http.StatusBadRequest, respn)
+		return
+	}
+	//validasi eksistensi user di db
+	docuser, err := atdb.GetOneDoc[model.Userdomyikado](config.Mongoconn, "user", primitive.M{"phonenumber": payload.Id})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Data user tidak di temukan"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusNotImplemented, respn)
+		return
+	}
+	//ambil data project
+	prjobjectId, err := primitive.ObjectIDFromHex(lap.Kode)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : ObjectID Tidak Valid"
+		respn.Info = lap.Kode
+		respn.Location = "Encode Object ID Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusBadRequest, respn)
+		return
+	}
+	prjuser, err := atdb.GetOneDoc[model.Project](config.Mongoconn, "project", primitive.M{"_id": prjobjectId})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Data project tidak di temukan: " + lap.Kode
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusNotImplemented, respn)
+		return
+	}
+	//lojik inputan post
+	lap.Project = prjuser
+	lap.User = docuser
+	lap.Phone = ValidasiNoHandPhone(lap.Phone)
+	lap.Petugas = docuser.Name
+	lap.NoPetugas = docuser.PhoneNumber
+	//memastikan nomor yang dimintai feedback bukan anggota
+	for _, member := range prjuser.Members {
+		if lap.Phone == member.PhoneNumber {
+			var respn model.Response
+			respn.Status = "Feedback tidak boleh dari sendiri atau rekan satu tim " + member.Name
+			respn.Response = member.PhoneNumber
+			at.WriteJSON(respw, http.StatusForbidden, respn)
+			return
+		}
+	}
+
+	idlap, err := atdb.InsertOneDoc(config.Mongoconn, "uxlaporan", lap)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Gagal Insert Database"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusNotModified, respn)
+		return
+	}
+	_, err = report.TambahPoinLaporanbyPhoneNumber(config.Mongoconn, prjuser, docuser.PhoneNumber, 1, "feedback")
+	if err != nil {
+		var resp model.Response
+		resp.Info = "TambahPoinLaporanbyPhoneNumber gagal"
+		resp.Response = err.Error()
+		at.WriteJSON(respw, http.StatusExpectationFailed, resp)
+		return
+	}
+	message := "*Permintaan Feedback*\n" + "Petugas : " + docuser.Name + "\nDeskripsi:" + lap.Solusi + "\n Beri Nilai: " + "https://www.do.my.id/rate/#" + idlap.Hex()
+	dt := &whatsauth.TextMessage{
+		To:       lap.Phone,
+		IsGroup:  false,
+		Messages: message,
+	}
+	_, resp, err := atapi.PostStructWithToken[model.Response]("Token", config.WAAPIToken, dt, config.WAAPIMessage)
+	if err != nil {
+		resp.Info = "Tidak berhak"
+		resp.Response = err.Error()
+		at.WriteJSON(respw, http.StatusUnauthorized, resp)
+		return
+	}
+	at.WriteJSON(respw, http.StatusOK, lap)
+
+}
+
+func ValidasiNoHandPhone(nomor string) string {
+	nomor = strings.ReplaceAll(nomor, " ", "")
+	nomor = strings.ReplaceAll(nomor, "+", "")
+	nomor = strings.ReplaceAll(nomor, "-", "")
+	return nomor
+}
