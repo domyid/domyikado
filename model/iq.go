22f8c26502174b4ca6415f4632d0658892078939
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-04-22 18:24:45 +0700

    merapihkan model iq

--- model/iq.go
+++ model/iq.go
@@ -6,18 +6,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// Struct SoalIQ sesuai dengan koleksi iqquestion di db MongoDB
 type SoalIQ struct {
-	ID        string  `json:"id" bson:"id"`
-	Question  string  `json:"question" bson:"question"`
-	Image     string  `json:"image" bson:"image"`
-	AnswerKey *string `json:"answer_key,omitempty" bson:"answer_key,omitempty"` // Pakai pointer agar bisa nil
-	CreatedAt string  `json:"created_at" bson:"created_at"`
+	ID        string  `json:"id" bson:"id,omitempty"`
+	Question  string  `json:"question" bson:"question,omitempty"`
+	Image     string  `json:"image" bson:"image,omitempty"`
+	AnswerKey *string `json:"answer_key,omitempty" bson:"answer_key,omitempty"`
+	CreatedAt string  `json:"created_at" bson:"created_at,omitempty"`
 	UpdatedAt *string `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 	DeletedAt *string `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
 }
 
-// Struct untuk menyimpan skor referensi dari iqscoring
 type IqScoring struct {
 	ID        string  `json:"id" bson:"id"`
 	Score     string  `json:"score" bson:"score"`
@@ -26,30 +24,29 @@ type IqScoring struct {
 	UpdatedAt *string `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
 
-// Struct untuk menyimpan data gabungan User + Score IQ
 type UserWithIqScore struct {
-	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Name        string             `json:"name,omitempty"`
-	PhoneNumber string             `json:"phonenumber,omitempty"`
-	Email       string             `json:"email,omitempty"`
-	Poin        float64            `json:"poin,omitempty"`
-	Score       string             `json:"score,omitempty"`
-	IQ          string             `json:"iq,omitempty"`
-	CreatedAt   string             `json:"created_at,omitempty"`
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	PhoneNumber string             `json:"phonenumber,omitempty" bson:"phonenumber,omitempty"`
+	Email       string             `json:"email,omitempty" bson:"email,omitempty"`
+	Poin        float64            `json:"poin,omitempty" bson:"poin,omitempty"`
+	Score       string             `json:"score,omitempty" bson:"score,omitempty"`
+	IQ          string             `json:"iq,omitempty" bson:"iq,omitempty"`
+	CreatedAt   string             `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
 
-// Struct untuk menyimpan hasil tes pengguna ke iqscore
 type IqScore struct {
 	ID          primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
 	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
 	PhoneNumber string             `json:"phonenumber,omitempty" bson:"phonenumber,omitempty"`
 	Score       string             `json:"score" bson:"score"`
 	IQ          string             `json:"iq" bson:"iq"`
+	WaGroupID   string             `json:"wagroupid" bson:"wagroupid"`
 	CreatedAt   string             `json:"created_at" bson:"created_at"`
 	UpdatedAt   *time.Time         `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
 
 type UserAnswer struct {
-	Name    string   `json:"name"`
-	Answers []string `json:"answers"` // Contoh: ["4", "2", "3", "TIDAK"]
+	Name    string   `json:"name" bson:"name,omitempty"`
+	Answers []string `json:"answers" bson:"answers,omitempty"` // ["4", "2", "3", "TIDAK"]
 }
