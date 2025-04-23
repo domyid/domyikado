5dcdc05aab3b86c0eab7e2c783a9b092618f8109
raffzhm <rafiazhim.ra@gmail.com>
2025-04-23 16:40:43 +0700

    coba tambah route

--- route/route.go
+++ route/route.go
@@ -288,6 +288,8 @@ func URL(w http.ResponseWriter, r *http.Request) {
 	case method == "GET" && path == "/refresh/report/gtmetrixmingguan":
 		controller.RefreshGTMetrixMingguanReport(w, r)
 
+	case method == "GET" && path == "/bukped/data":
+		controller.GetDataBukpedMember()
 	//strava coba
 	case method == "GET" && path == "/data/strava": // hanya untuk mengambil data strava lama
 		controller.ProcessStravaPoints(w, r)
