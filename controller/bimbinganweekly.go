9ea56b3c17031a2f06719376f51959698af26076
“GilarYa” <gilarwahibul@gmail.com>
2025-04-26 10:58:39 +0700

    del some func

--- controller/bimbinganweekly.go
+++ controller/bimbinganweekly.go
@@ -229,7 +229,6 @@ func calculateDifferentialPoints(phoneNumber string, weekNumber int, currentActi
 		return currentActivityScore, nil
 	}
 
-	// Inisialisasi dengan nol
 	var previousTotalScore = model.ActivityScore{
 		Sponsordata:     0,
 		Sponsor:         0,
