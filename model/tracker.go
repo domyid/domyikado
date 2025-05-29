3913c92e81e7e015a7013ac86bc4056ee809572d
Ibrohim Mubarok <befousmain@gmail.com>
2025-05-29 20:03:19 +0700

    Benerin type

--- model/tracker.go
+++ model/tracker.go
@@ -3,15 +3,15 @@ package model
 import "time"
 
 type UserInfo struct {
-	Hostname          string    `json:"hostname"`
-	Url               string    `json:"url"`
-	Browser           string    `json:"browser"`
-	Browser_Language  string    `json:"browser_language"`
-	Screen_Resolution string    `json:"screen_resolution"`
-	Timezone          string    `json:"timezone"`
-	Ontouchstart      string    `json:"ontouchstart"`
-	Tanggal_Ambil     time.Time `json:"tanggal_ambil"`
-	ISP               ISP       `json:"isp"`
+	Hostname          string    `json:"hostname" bson:"hostname"`
+	Url               string    `json:"url" bson:"url"`
+	Browser           string    `json:"browser" bson:"browser"`
+	Browser_Language  string    `json:"browser_language" bson:"browser_language"`
+	Screen_Resolution string    `json:"screen_resolution" bson:"screen_resolution"`
+	Timezone          string    `json:"timezone" bson:"timezone"`
+	OnTouchStart      string    `json:"ontouchstart" bson:"ontouchstart"`
+	Tanggal_Ambil     time.Time `json:"tanggal_ambil" bson:"tanggal_ambil"`
+	ISP               ISP       `json:"isp" bson:"isp"`
 }
 
 type HostnameTanggal struct {
@@ -25,13 +25,14 @@ type PhoneDomain struct {
 }
 
 type ISP struct {
-	IP           string    `json:"ip" bson:"ip"`
-	City         time.Time `json:"city" bson:"city"`
-	Region       time.Time `json:"region" bson:"region"`
-	Country_Name time.Time `json:"country_name" bson:"country_name"`
-	Postal       time.Time `json:"postal" bson:"postal"`
-	Latitude     time.Time `json:"latitude" bson:"latitude"`
-	Longitude    time.Time `json:"longitude" bson:"longitude"`
-	Timezone     time.Time `json:"timezone" bson:"timezone"`
-	Org          time.Time `json:"org" bson:"org"`
+	IP           string  `json:"ip" bson:"ip"`
+	City         string  `json:"city,omitempty" bson:"city,omitempty"`
+	Region       string  `json:"region,omitempty" bson:"region,omitempty"`
+	Country_Name string  `json:"country_name" bson:"country_name"`
+	Postal       string  `json:"postal,omitempty" bson:"postal,omitempty"`
+	Latitude     float64 `json:"latitude,omitempty" bson:"latitude,omitempty"`
+	Longitude    float64 `json:"longitude,omitempty" bson:"longitude,omitempty"`
+	Timezone     string  `json:"timezone,omitempty" bson:"timezone,omitempty"`
+	Asn          string  `json:"asn,omitempty" bson:"asn,omitempty"`
+	Org          string  `json:"org,omitempty" bson:"org,omitempty"`
 }
