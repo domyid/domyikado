3758a9701cd41a2471a8162f70b01b9ffb2a6b3e
Ibrohim Mubarok <befousmain@gmail.com>
2025-05-29 19:36:41 +0700

    Ganti model

--- model/tracker.go
+++ model/tracker.go
@@ -3,11 +3,15 @@ package model
 import "time"
 
 type UserInfo struct {
-	IPv4          string    `json:"ipv4"`
-	Hostname      string    `json:"hostname"`
-	Url           string    `json:"url"`
-	Browser       string    `json:"browser"`
-	Tanggal_Ambil time.Time `json:"tanggal_ambil"`
+	Hostname          string    `json:"hostname"`
+	Url               string    `json:"url"`
+	Browser           string    `json:"browser"`
+	Browser_Language  string    `json:"browser_language"`
+	Screen_Resolution string    `json:"screen_resolution"`
+	Timezone          string    `json:"timezone"`
+	Ontouchstart      string    `json:"ontouchstart"`
+	Tanggal_Ambil     time.Time `json:"tanggal_ambil"`
+	ISP               time.Time `json:"isp"`
 }
 
 type HostnameTanggal struct {
@@ -18,4 +22,16 @@ type HostnameTanggal struct {
 type PhoneDomain struct {
 	PhoneNumber      string
 	Project_Hostname string
+}
+
+type ISP struct {
+	IP           string    `json:"ip" bson:"ip"`
+	City         time.Time `json:"city" bson:"city"`
+	Region       time.Time `json:"region" bson:"region"`
+	Country_Name time.Time `json:"country_name" bson:"country_name"`
+	Postal       time.Time `json:"postal" bson:"postal"`
+	Latitude     time.Time `json:"latitude" bson:"latitude"`
+	Longitude    time.Time `json:"longitude" bson:"longitude"`
+	Timezone     time.Time `json:"timezone" bson:"timezone"`
+	Org          time.Time `json:"org" bson:"org"`
 }
